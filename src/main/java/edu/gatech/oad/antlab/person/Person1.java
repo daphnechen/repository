55030be8bffb87bf486768c614fdaef7326c5b37
vamzzz <vamshi97@yahoo.com>
2017-01-23 18:36:54 -0500

    Person1 is Vamshi Adimulam.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,11 +1,11 @@
-package edu.gatech.oad.antlab.person;
+package main.java.edu.gatech.oad.antlab.person;
 
 /**
  *  A simple class for person 1
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Vamshi Adimulam
  *  @version 1.1
  */
 public class Person1 {
