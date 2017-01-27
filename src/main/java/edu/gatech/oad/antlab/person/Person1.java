b682375bb0804a077bc70268f52be6b77e92afc2
vamzzz <vamshi97@yahoo.com>
2017-01-27 16:36:11 -0500

    Implemented calc method in Person 1.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,13 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+		String returned = "";
+		for (int x = 2; x < input.length(); x++) {
+			returned += input.charAt(x);
+		}
+		returned += input.charAt(0);
+		returned += input.charAt(1);
+	  return returned;
 	}
 	
 	/**
